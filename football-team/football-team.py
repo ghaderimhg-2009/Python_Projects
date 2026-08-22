b0b9013355a8a3b29beca5b714d6600221d185ba
Mohammad Hosein Ghaderi <ghaderi.de.mhg@gmail.com>
2026-08-22 12:54:36 +0330

    Add football-team.py

---
+++ football-team/football-team.py
@@ -0,0 +1,22 @@
+team = [
+    "ali",
+    "amir",
+    "ahmad",
+    "mahdi",
+    "reza",
+    "hosein"
+]
+
+team.remove("ahmad")
+print(f"list 1:{team} \n")
+
+team.append("ronaldo")
+print(f"list 2:{team} \n")
+
+team.sort()
+print(f"list 3:{team} \n")
+
+team.reverse()
+print(f"list 4:{team} \n")
+
+print(f"list 5: {len(team)}")
